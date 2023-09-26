7742747e15636dd2de296340793bd0728e43e8eb
RVanVlietII <rcvanvlietii@gmail.com>
2023-09-26 18:31:52 -0400

    modifications to server.js, aded api get routes and post routes and delete routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const uuid = require('./helpers/uuid');
+const api = require('./routes/index.js');
+const db = require('./db/db');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,13 +10,20 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+// app.use(express.static('public'));
+
 // GET Route for homepage
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
-//GET Route for notes in db
-app.get('/api/notes', (req, res) => {
-  res.status(200).json(db);
+// GET Route for notes in db
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    // res.status(200).json(db);
 });
 
 //POST Route for sending notes on request
@@ -22,15 +31,15 @@ app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to post note`);
 
     // Destructuring assignment for the items in req.body
-    const { note, username } = req.body;
+    const { title, text } = req.body;
   
     // If all the required properties are present
-    if (product && username) {
+    if (title && text) {
       // Variable for the object we will save
       const newNote = {
-        note,
-        username,
-        review_id: uuid(),
+        title,
+        text,
+        note_id: uuid(),
       };
   
       const response = {
@@ -43,6 +52,64 @@ app.post('/api/notes', (req, res) => {
     } else {
       res.status(500).json('Error in posting review');
     }
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading notes:", err);
+            return res.sendStatus(500);
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading notes:", err);
+            return res.sendStatus(500);
+        }
+        const allNotes = JSON.parse(data);
+        allNotes.push(newNote);
+
+        fs.writeFile('db/db.json', JSON.stringify(allNotes), (err) => {
+            if (err) {
+                console.error("Error saving note:", err);
+                return res.sendStatus(500);
+            }
+            res.json(newNote);
+        });
+    });
+});
+
+app.delete('/api/notes/:index', (req, res) => {
+    const noteIndex = parseInt(req.params.index, 10);
+
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading notes:", err);
+            return res.sendStatus(500);
+        }
+        const allNotes = JSON.parse(data);
+        
+        if (noteIndex < 0 || noteIndex >= allNotes.length) {
+            return res.status(400).send("Invalid note index");
+        }
+        
+        allNotes.splice(noteIndex, 1); // Removes the note at the given index
+        
+        fs.writeFile('db/db.json', JSON.stringify(allNotes), (err) => {
+            if (err) {
+                console.error("Error deleting note based on index:", err);
+                return res.sendStatus(500);
+            }
+            res.json({ message: "Note deleted based on index!" });
+        });
+    });
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
